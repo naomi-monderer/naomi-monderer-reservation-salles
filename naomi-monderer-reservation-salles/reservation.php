b057f9f2da40eeefebd43c4cbdaaf54fea69489c
sofiane-ziri <91732738+sofiane-ziri@users.noreply.github.com>
2022-02-18 17:57:45 +0100

    form style

--- naomi-monderer-reservation-salles/reservation.php
+++ naomi-monderer-reservation-salles/reservation.php
@@ -1,4 +1,3 @@
-
 <?php
 
 session_start();
@@ -8,15 +7,10 @@ require_once('Class/User.php');
 require('Include/header.php');
 $oneResa = new Reservation();
 $oneReservation = $oneResa->displayReservation();
-<<<<<<< HEAD:naomi-monderer-reservation-salles/reservation.php
-
-
-=======
->>>>>>> 5b61d34b53cab4d6101282b791d6380429a9f05b:reservation.php
 ?>
 
 <h1>Réservé par <?= $oneReservation[0]['login'] ?> </h1>
-    <p>TITRE -  <?= $oneReservation[0]['titre'] ?></p>
-    <p>DESCRIPTION - <?= $oneReservation[0]['description'] ?></p>
-    <p>DEBUT - <?= $oneReservation[0]['debut'] ?></p>
-    <p>FIN - <?= $oneReservation[0]['fin'] ?></p>
+<p>TITRE - <?= $oneReservation[0]['titre'] ?></p>
+<p>DESCRIPTION - <?= $oneReservation[0]['description'] ?></p>
+<p>DEBUT - <?= $oneReservation[0]['debut'] ?></p>
+<p>FIN - <?= $oneReservation[0]['fin'] ?></p>
