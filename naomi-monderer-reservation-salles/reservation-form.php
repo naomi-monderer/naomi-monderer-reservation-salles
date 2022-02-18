d5a9f6b644c2b1ef3f5c543300378a8ded3fe72c
sofiane-ziri <91732738+sofiane-ziri@users.noreply.github.com>
2022-02-18 18:47:16 +0100

    resa_form

--- naomi-monderer-reservation-salles/reservation-form.php
+++ naomi-monderer-reservation-salles/reservation-form.php
@@ -40,47 +40,46 @@ if (isset($_POST['submit'])) {
 
 ?>
 <main>
-
-    <h2><?= $_GET['date'] ?></h2>
-
-    <form action="" method="post">
-        <div class="form-group">
-            <label for="titre">Titre du film:</label>
-            <input type="text" class="form-control" name="titre" placeholder="ex:Die Hard 3">
-
+    <div class="login-form">
+        <form action="" method="post">
+            <h2><?= $_GET['date'] ?></h2>
             <div class="form-group">
-                <label for="description">Description:</label>
-                <textarea name="description" class="form-control"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="debut">De:</label>
-                <?php
-                if (isset($_GET['date'])) {
-                    $date_debut = date("Y-m-d", strtotime($_GET['date']));
-                    $heure_debut = date("H:i", strtotime($_GET['date']));
-                    $event_debut = $date_debut . 'T' . $heure_debut;
-                ?>
+                <label for="titre">Titre du film:</label>
+                <input type="text" class="form-control" name="titre" placeholder="ex:Die Hard 3">
 
-                    <input type="datetime-local" class="form-control" name="debut" value="<?= $event_debut ?>">
-                <?php } else { ?>
-                    <input type="datetime-local" name="debut">
-                <?php }
-                $date_fin = date("Y-m-d", strtotime($_GET['date']));
-                $getDate = $_GET['date'] . "+1hour";
-                $heure_fin = date("H:i", strtotime($getDate));
-                $event_fin = $date_fin . 'T' . $heure_fin;
-                ?>
+                <div class="form-group">
+                    <label for="description">Description:</label>
+                    <textarea name="description" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="debut">De:</label>
+                    <?php
+                    if (isset($_GET['date'])) {
+                        $date_debut = date("Y-m-d", strtotime($_GET['date']));
+                        $heure_debut = date("H:i", strtotime($_GET['date']));
+                        $event_debut = $date_debut . 'T' . $heure_debut;
+                    ?>
 
-                <label for="fin">jusqu'à :</label>
-                <input type="datetime-local" class="form-control" name="fin" value="<?= $event_fin ?>">
-            </div>
+                        <input type="datetime-local" class="form-control" name="debut" value="<?= $event_debut ?>">
+                    <?php } else { ?>
+                        <input type="datetime-local" name="debut">
+                    <?php }
+                    $date_fin = date("Y-m-d", strtotime($_GET['date']));
+                    $getDate = $_GET['date'] . "+1hour";
+                    $heure_fin = date("H:i", strtotime($getDate));
+                    $event_fin = $date_fin . 'T' . $heure_fin;
+                    ?>
 
-            <input type="submit" class="btn btn-primary" name="submit">
+                    <label for="fin">jusqu'à :</label>
+                    <input type="datetime-local" class="form-control" name="fin" value="<?= $event_fin ?>">
+                </div>
+
+                <input type="submit" class="btn btn-primary" name="submit">
 
 
-    </form>
+        </form>
 
-    <?php echo $error; ?>
+        <?php echo $error; ?>
 </main>
 <?php
 
