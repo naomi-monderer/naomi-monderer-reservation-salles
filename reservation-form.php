b1d0beee8fe385bfbdf08377a5ecb07fa40ac40a
sofiane-ziri <91732738+sofiane-ziri@users.noreply.github.com>
2022-02-21 18:25:11 +0100

    disable

--- reservation-form.php
+++ reservation-form.php
@@ -53,38 +53,40 @@ if (isset($_POST['submit'])) {
             </div>
             <div class="form-group">
                 <label for="debut">De:</label>
-                    <?php
-                    if (isset($_GET['date'])) {
-                        $date_debut = date("Y-m-d", strtotime($_GET['date']));
-                        $heure_debut = date("H:i", strtotime($_GET['date']));
-                        $event_debut = $date_debut . 'T' . $heure_debut;
-                    ?>
+                <?php
+                if (isset($_GET['date'])) {
+                    $date_debut = date("Y-m-d", strtotime($_GET['date']));
+                    $heure_debut = date("H:i", strtotime($_GET['date']));
+                    $event_debut = $date_debut . 'T' . $heure_debut;
+                ?>
 
-                <input type="datetime-local" class="form-control" name="debut" value="<?= $event_debut ?>">
-                    <?php } else { ?>
-                        <input type="datetime-local" name="debut">
-                    <?php }
-                    // permet d'insérer seulement un creneau d'une heure 
-                    $date_fin = date("Y-m-d", strtotime($_GET['date']));
-                    $getDate = $_GET['date'] . "+1hour";
-                    $heure_fin = date("H:i", strtotime($getDate));
+                    <input type="datetime-local" class="form-control" name="debut" value="<?= $event_debut ?>" style="pointer-events: none;">
 
-                    $event_fin = $date_fin . 'T' . $heure_fin;
-                    ?>
+
+                <?php } else { ?>
+                    <input type="datetime-local" name="debut">
+                <?php }
+                // permet d'insérer seulement un creneau d'une heure 
+                $date_fin = date("Y-m-d", strtotime($_GET['date']));
+                $getDate = $_GET['date'] . "+1hour";
+                $heure_fin = date("H:i", strtotime($getDate));
+
+                $event_fin = $date_fin . 'T' . $heure_fin;
+                ?>
 
                 <label for="fin">jusqu'à :</label>
-                <input type="datetime-local" class="form-control" name="fin" value="<?= $event_fin ?>">
+                <input type="datetime-local" class="form-control" name="fin" value="<?= $event_fin ?>" style="pointer-events: none;">
             </div>
             <div>
-                    <input type="submit" class="btn btn-primary" value="Réserver" name="submit">
-                    <?php if (isset($_POST["submit"])) {
-                        header('Location: planning.php');
-                        die();
-                    } ?>
-            </div>        
+                <input type="submit" class="btn btn-primary" value="Réserver" name="submit">
+                <?php if (isset($_POST["submit"])) {
+                    header('Location: planning.php');
+                    die();
+                } ?>
+            </div>
         </form>
         <?php echo $error; ?>
-    </div>    
+    </div>
 </main>
 <?php
 require('Include/footer.php');
